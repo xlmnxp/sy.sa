6fff89fad2d9b3357c52d09ff0aced37cfd01894
Salem Yaslem <s@sy.sa>
2024-10-12 22:45:49 +0300

    fixing CSS issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
-  experimental: {
-		inlineSSRStyles: false,
-	},
   postcss: {
     plugins: {
       tailwindcss: {},
