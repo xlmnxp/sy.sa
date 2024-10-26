ef20e316d5f968ae871e1746a719bc3c7940b9cf
Salem Yaslem <s@sy.sa>
2024-10-26 05:46:41 +0300

    fix drop-down list arrow of sort input

--- components/RepoSearch.vue
+++ components/RepoSearch.vue
@@ -12,12 +12,19 @@
             </svg>
         </div>
 
-        <select :value="sortBy" @change="$emit('update:sortBy', ($event.target as HTMLSelectElement).value)"
-            class="px-4 py-2 rounded-lg border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-gray-900 dark:text-white focus:ring-2 focus:ring-primary dark:focus:ring-primary-dark focus:border-transparent">
-            <option value="stars">Sort by Stars</option>
-            <option value="name">Sort by Name</option>
-            <option value="updated">Sort by Last Updated</option>
-        </select>
+        <div class="relative">
+            <select :value="sortBy" @change="$emit('update:sortBy', ($event.target as HTMLSelectElement).value)"
+                class="appearance-none px-4 py-2 pr-10 rounded-lg border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-gray-900 dark:text-white focus:ring-2 focus:ring-primary dark:focus:ring-primary-dark focus:border-transparent">
+                <option value="stars">Sort by Stars</option>
+                <option value="name">Sort by Name</option>
+                <option value="updated">Sort by Last Updated</option>
+            </select>
+            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 dark:text-gray-300">
+                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
+                </svg>
+            </div>
+        </div>
     </div>
 </template>
 
