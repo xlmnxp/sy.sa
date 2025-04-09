961230fc4fd51b45acaed162de54f562a5cc80d2
Salem Yaslem <s@sy.sa>
2025-04-09 10:15:38 +0000

    Add conditional rendering for stargazers and forks count in RepoCard component

--- components/RepoCard.vue
+++ components/RepoCard.vue
@@ -8,14 +8,14 @@
                 </a>
             </h2>
             <div class="flex items-center">
-                <span class="flex items-center text-gray-600 dark:text-gray-400 ms-2">
+                <span v-if="repo.stargazers_count" class="flex items-center text-gray-600 dark:text-gray-400 ms-2">
                     <svg class="w-4 h-4 me-1" fill="currentColor" viewBox="0 0 16 16">
                         <path
                             d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.75.75 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25z" />
                     </svg>
                     {{ repo.stargazers_count }}
                 </span>
-                <span class="flex items-center text-gray-600 dark:text-gray-400 ms-2">
+                <span v-if="repo.forks_count" class="flex items-center text-gray-600 dark:text-gray-400 ms-2">
                     <svg class="w-4 h-4 me-1" fill="currentColor" viewBox="0 0 16 16">
                         <path
                             d="M5 5.372v.878c0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75v-.878a2.25 2.25 0 1 1 1.5 0v.878a2.25 2.25 0 0 1-2.25 2.25h-1.5v2.128a2.251 2.251 0 1 1-1.5 0V8.5h-1.5A2.25 2.25 0 0 1 3.5 6.25v-.878a2.25 2.25 0 1 1 1.5 0ZM5 3.25a.75.75 0 1 0-1.5 0 .75.75 0 0 0 1.5 0Zm6.75.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm-3 8.75a.75.75 0 1 0-1.5 0 .75.75 0 0 0 1.5 0Z" />
