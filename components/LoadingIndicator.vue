d41429c4c2646767509391bf0bc461ba7050f2d1
Salem Yaslem <s@sy.sa>
2025-04-10 08:30:37 +0300

    Fix loading indicator on dark mode

--- components/LoadingIndicator.vue
+++ components/LoadingIndicator.vue
@@ -5,7 +5,7 @@
                 class="w-16 h-16 border-4 border-primary dark:border-primary-dark border-t-transparent rounded-full animate-spin">
             </div>
             <div
-                class="w-12 h-12 border-4 border-primary/60 dark:border-primary-dark/60 border-t-transparent rounded-full animate-spin absolute top-2 left-2">
+                class="w-12 h-12 border-4 border-primary/60 dark:border-primary-dark/60 border-t-transparent dark:border-t-transparent rounded-full animate-spin absolute top-2 left-2">
             </div>
         </div>
         <p class="text-gray-600 dark:text-gray-400 animate-pulse">{{ $t("repos.loading") }}</p>
